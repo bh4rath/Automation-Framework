f20ec87410843096d11020174c2714ef0b6e2225
Bharath S <bharathgates33@gmail.com>
2020-05-19 17:19:14 +0530

    Update ExtentReportGeneration.java

--- src/main/java/utilities/ExtentReportGeneration.java
+++ src/main/java/utilities/ExtentReportGeneration.java
@@ -2,6 +2,7 @@ package utilities;
 
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+import com.aventstack.extentreports.reporter.configuration.Theme;
 
 public class ExtentReportGeneration {
 	public static ExtentReports extent;
